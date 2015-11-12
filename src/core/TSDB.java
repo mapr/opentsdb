cf7de623e77a864cd4d9c6d733ae08f92a5215e2
ntirupattur@maprtech.com <ntirupattur@maprtech.com>
2015-11-11 22:04:52 -0800

    Fixing build

--- src/core/TSDB.java
+++ src/core/TSDB.java
@@ -53,6 +53,7 @@ import net.opentsdb.stats.StatsCollector;
 
 import org.apache.hadoop.conf.Configuration;
 import com.mapr.fs.MapRFileSystem;
+import org.apache.hadoop.fs.FileSystem
 
 /**
  * Thread-safe implementation of the TSDB client.
