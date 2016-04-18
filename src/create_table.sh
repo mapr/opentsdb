14afbf3f204c14682faf8f45b2057a3779493f50
ntirupattur@maprtech.com <ntirupattur@maprtech.com>
2016-04-18 12:07:15 -0700

    Simplify CF creation

--- src/create_table.sh
+++ src/create_table.sh
@@ -8,40 +8,37 @@ META_TABLE=${META_TABLE:-'/tsdb-meta'}
 LOGFILE=${OT_HOME}/var/log/opentsdb/opentsdb_create_table_$$.log
 
 for t in $TSDB_TABLE $UID_TABLE $TREE_TABLE $META_TABLE; do
-   maprcli table info -path $t> $LOGFILE 2>&1
-   RC1=$?
-   if [ $RC1 -ne 0 ]; then
-     echo "Creating $t table..."
-     maprcli table create -path $t -defaultreadperm p -defaultwriteperm p -defaultappendperm p > $LOGFILE 2>&1
-     RC2=$?
-     if [ $RC2 -ne 0]; then
-       echo "Create table failed for $t"
-       return $RC2 2>/dev/null || exit $RC2
-     fi
-     if [ "$t" == "$UID_TABLE" ]; then
-       for columnFamily in "id" "name"; do
-         echo "Creating CF $columnFamily for Table $t"
-         maprcli table cf create -path $t -cfname $columnFamily -maxversions 1 -inmemory true -compression lzf -ttl 0 > $LOGFILE 2>&1
-         RC2=$?
-         if [ $RC2 -ne 0]; then
-           echo "Create CF $columnFamily failed for table $t"
-           return $RC2 2>/dev/null || exit $RC2
-         fi
-       done
-     elif
-       if [ "$t" == "$META_TABLE" ]; then
-         columnFamily="name"
-       elif
-         columnFamily="t"
-       fi
-       echo "Creating CF $columnFamily for Table $t"
-       maprcli table cf create -path $t -cfname $columnFamily -maxversions 1 -inmemory false -compression lzf -ttl 0 > $LOGFILE 2>&1
-       RC2=$?
-       if [ $RC2 -ne 0]; then
-         echo "Create CF $columnFamily failed for table $t"
-         return $RC2 2>/dev/null || exit $RC2
-       fi
+  maprcli table info -path $t> $LOGFILE 2>&1
+  RC1=$?
+  if [ $RC1 -ne 0 ]; then
+    echo "Creating $t table..."
+    maprcli table create -path $t -defaultreadperm p -defaultwriteperm p -defaultappendperm p > $LOGFILE 2>&1
+    RC2=$?
+    if [ $RC2 -ne 0]; then
+      echo "Create table failed for $t"
+      return $RC2 2>/dev/null || exit $RC2
     fi
+    if [ "$t" == "$UID_TABLE" ]; then
+      COLUMNS="id name"
+    elif [ "$t" == "$META_TABLE" ]; then
+      COLUMNS="name"
+    else
+      COLUMNS="t"
+    fi
+    for columnFamily in $COLUMNS ; do
+      echo "Creating CF $columnFamily for Table $t"
+      if [ "$t" == "$UID_TABLE" ]; then
+        flag="true"
+      else
+        flag="false"
+      fi 
+      maprcli table cf create -path $t -cfname $columnFamily -maxversions 1 -inmemory $flag -compression lzf -ttl 0 > $LOGFILE 2>&1
+      RC2=$?
+      if [ $RC2 -ne 0]; then
+        echo "Create CF $columnFamily failed for table $t"
+        return $RC2 2>/dev/null || exit $RC2
+      fi
+    done
   else
     echo "$t exists."
   fi
